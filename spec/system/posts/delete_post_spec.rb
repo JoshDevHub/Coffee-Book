4f8aa709fc51e278c60635feb8d1d1c45d4e8f82
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-21 14:49:10 -0600

    test: add system test for post deletion

---
+++ spec/system/posts/delete_post_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Deleting a post", type: :system do
+  let(:user) { User.create(email: "test@example.com", password: "password123") }
+  let(:post) { Post.create(body: "Test post", user: user) }
+
+  before do
+    login_as(user)
+  end
+
+  it "Deletes the post" do
+    visit post_path(post)
+    click_on "Delete Post"
+    expect(page).not_to have_content("Test post")
+  end
+end
