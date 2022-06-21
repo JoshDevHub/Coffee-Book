31649d5cdd243f1241152cedb58ff33c84bea62c
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-21 13:04:14 -0600

    feat: create CRUD controller actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,50 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = current_user.posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
   end
 end
