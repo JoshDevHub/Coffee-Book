284043e870d1a69974fc39562f98f71a1b2120fa
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-22 21:17:02 -0600

    feat: build associations with new FriendRequest model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :sent_friend_requests, class_name: "FriendRequest",
+                                  foreign_key: :sender_id,
+                                  inverse_of: :sender,
+                                  dependent: :destroy
+  has_many :received_friend_requests, class_name: "FriendRequest",
+                                      foreign_key: :receiver_id,
+                                      inverse_of: :receiver,
+                                      dependent: :destroy
 end
