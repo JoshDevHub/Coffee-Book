afbef780e56ecb66ccdec4f3c75d5ed1a5e91008
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-24 22:52:05 -0600

    refactor: cleanup NotificationsController method
    Use a better practice for getting the user params

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,13 @@
 class NotificationsController < ApplicationController
+  before_action :user_param
+
   def index
-    @notifications = Notification.where(user_id: params[:user_id])
+    @notifications = @user.notifications
+  end
+
+  private
+
+  def user_param
+    @user = User.find(params[:user_id])
   end
 end
