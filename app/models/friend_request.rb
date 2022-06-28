337d68bbd06252e74de20706ed65b8047072e90d
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-27 20:31:16 -0600

    refactor: change relation to Notification to has_one

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -2,6 +2,8 @@ class FriendRequest < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 
+  has_one :notification, dependent: :destroy
+
   scope :pending, -> { where(accepted: false) }
   scope :confirmed, -> { where(accepted: true) }
 
