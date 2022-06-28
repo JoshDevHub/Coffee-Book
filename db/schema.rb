6fe05cde31c8c2c073e38fa4f05eff98f2f347f4
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-27 22:50:38 -0600

    feat: add User reference to Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_24_155958) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_044621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "friend_requests", force: :cascade do |t|
     t.bigint "sender_id", null: false
@@ -56,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_155958) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "friend_requests", "users", column: "receiver_id"
   add_foreign_key "friend_requests", "users", column: "sender_id"
   add_foreign_key "notifications", "friend_requests"
