f4be06c6911523ec84c8d13121da8ebaaf9094d7
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-28 19:52:44 -0600

    feat: tell user if request they're viewing has been accepted

--- spec/system/friend_requests/confirm_request_spec.rb
+++ spec/system/friend_requests/confirm_request_spec.rb
@@ -4,16 +4,33 @@ RSpec.describe "Confirming a friend request", type: :system do
   let(:sending_user) { create(:user) }
   let(:accepting_user) { create(:user) }
 
-  before do
-    sending_user.send_friend_request(accepting_user)
-    login_as(accepting_user)
+  context "when the friend request is pending" do
+    before do
+      sending_user.send_friend_request(accepting_user)
+      login_as(accepting_user)
+    end
+
+    it "accepts the friend request" do
+      visit user_notifications_path(accepting_user)
+      click_on "Friend Request"
+      click_on "Confirm Request"
+
+      expect(page).to have_content("Friend Added")
+    end
   end
 
-  it "accepts the friend request" do
-    visit user_notifications_path(accepting_user)
-    click_on "Friend Request"
-    click_on "Confirm Request"
+  context "when the friend request has been accepted" do
+    before do
+      sending_user.send_friend_request(accepting_user)
+      accepting_user.received_friend_requests.first.confirm
+      login_as(accepting_user)
+    end
 
-    expect(page).to have_content("Friend Added")
+    it "tells the user that they already have this friendship" do
+      visit user_notifications_path(accepting_user)
+      click_on "Friend Request"
+
+      expect(page).to have_content("You are already friends")
+    end
   end
 end
