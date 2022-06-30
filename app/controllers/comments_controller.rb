292e5a8c01aacb6a5065ef231c7c0c253a28aaf1
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-30 10:04:05 -0600

    feat: add flash messages for `comment#create`
    Also refactor flash message display in application layout view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,11 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
+    if @comment.save
+      flash[:success] = "Your comment has been added"
+    else
+      flash[:error] = "Comment body cannot be blank"
+    end
     redirect_to post_path(@post)
   end
 
