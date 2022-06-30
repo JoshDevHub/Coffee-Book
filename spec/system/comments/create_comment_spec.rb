75228721f807d44c5756bf7759777ecc176ae3e3
jmsmith1018 <jmsmith1018@gmail.com>
2022-06-30 10:05:30 -0600

    test: add system test for creating comments

---
+++ spec/system/comments/create_comment_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Creating a comment", type: :system do
+  let(:user) { create(:user) }
+
+  before do
+    login_as(user)
+    create(:post, user: user)
+  end
+
+  context "when the inputs are valid" do
+    it "creates a new comment" do
+      visit root_path
+      click_on "View Post"
+      fill_in "Body", with: "Test Comment"
+      click_on "Create Comment"
+
+      expect(page).to have_content("Test Comment")
+    end
+  end
+
+  context "when the inputs are not valid" do
+    it "renders an error message" do
+      error_message = "Comment body cannot be blank"
+      visit root_path
+      click_on "View Post"
+      click_on "Create Comment"
+
+      expect(page).to have_content(error_message)
+    end
+  end
+end
