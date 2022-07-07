ba464229ad38c81e16b3739c62323839f3842bba
jmsmith1018 <jmsmith1018@gmail.com>
2022-07-07 15:49:38 -0600

    feat: add `#create` action for FriendRequests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -7,6 +7,13 @@ class FriendRequestsController < ApplicationController
     @friend_request = FriendRequest.find(params[:id])
   end
 
+  def create
+    @friend_request = current_user.sent_friend_requests.build(friend_request_params)
+    flash[:success] = "Friend Request Sent" if @friend_request.save
+
+    redirect_to users_path
+  end
+
   def destroy
     @friend_request = FriendRequest.find(params[:id])
     @friend_request.destroy
@@ -19,5 +26,11 @@ class FriendRequestsController < ApplicationController
     @friend_request.confirm
     flash[:notice] = "Friend Added"
     redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def friend_request_params
+    params.require(:friend_request).permit(:sender, :receiver_id)
   end
 end
