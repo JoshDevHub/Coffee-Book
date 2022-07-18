216289ab7daef68a5bdad4f22452f15a747856a4
jmsmith1018 <jmsmith1018@gmail.com>
2022-07-18 14:09:49 -0600

    feat: build `profile#edit` controller action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,23 @@ class ProfilesController < ApplicationController
     @profile = Profile.find_by(user_id: params[:user_id])
   end
 
-  def edit; end
+  def edit
+    @profile = Profile.find_by(user_id: params[:user_id])
+  end
 
-  def update; end
+  def update
+    @profile = Profile.find(params[:user_id])
+
+    if @profile.update(profile_params)
+      redirect_to user_profile_path(current_user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:gender, :birthday, :country)
+  end
 end
