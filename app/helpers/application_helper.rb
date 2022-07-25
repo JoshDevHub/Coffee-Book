282e56201a07cb7a9505cc9f5e127fac006ab56d
jmsmith1018 <jmsmith1018@gmail.com>
2022-07-25 12:26:03 -0600

    feat: add helper for svg tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def svg(name)
+    file_path = Rails.root.join("app/assets/images/#{name}.svg")
+    return File.read(file_path).html_safe if File.exist?(file_path)
+
+    "(not found)"
+  end
 end
