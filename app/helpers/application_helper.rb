30d5e9e45c76e195acb8ab408514573c6eebdffa
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-01 14:13:24 -0600

    chore: remove old svg helper method -- replaced by `inline_svg`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def svg(name)
-    file_path = Rails.root.join("app/assets/images/#{name}.svg")
-    return File.read(file_path).html_safe if File.exist?(file_path)
-
-    "(not found)"
-  end
 end
