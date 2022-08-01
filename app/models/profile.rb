ef2ce13c6f2e28dad74de9378050ae31f70d2291
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-01 14:26:00 -0600

    refactor: build gender enum as constant
    This allows this to be accessed in a more intention revealing way
    in the view layer.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -19,12 +19,14 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Profile < ApplicationRecord
-  enum gender: {
+  GENDERS = {
     unspecified: 0,
     male: 1,
     female: 2,
     nonbinary: 3
-  }
+  }.freeze
+
+  enum gender: GENDERS
 
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_fill: [80, 80]
