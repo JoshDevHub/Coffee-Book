8d3a49d9faac72625ce4423c3ab3ff1abf12eb47
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-04 14:30:43 -0600

    fix: fix issue with gravatar sizing too small

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
-  def gravatar_url_for(user, size: 80)
+  def gravatar_url_for(user, size:)
+    size = { thumb: 80, icon: 48 }.fetch(size, 80)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
   end
