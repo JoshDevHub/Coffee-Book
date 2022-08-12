2e8e26f7529b1b8337b20ddbfdd2e0b83f8d3b45
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-11 21:03:20 -0600

    feat: mark user notifications as read when visiting notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   before_action :user_param
+  before_action :user_read_notifications, only: :index
 
   def index
     @notifications = @user.notifications.includes(:friendship)
@@ -9,5 +10,9 @@ class NotificationsController < ApplicationController
 
   def user_param
     @user = User.find(params[:user_id])
+  end
+
+  def user_read_notifications
+    current_user.read_notifications
   end
 end
