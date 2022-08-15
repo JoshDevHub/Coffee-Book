e0df5ad841a8a3e3a294c8775233f5580ba4ba23
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-15 11:23:11 -0600

    feat: create action for welcome email to new users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "notifications@example.com"
+
+  def welcome_email
+    @user = params[:user]
+    @url = "<placeholder.com>"
+    mail(to: @user.email, subject: "Welcome to Coffee Book!")
+  end
 end
