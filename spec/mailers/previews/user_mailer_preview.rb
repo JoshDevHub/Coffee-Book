6040e56f2ac2c4b6b979a626bbb3d3e17576fd97
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-15 12:49:41 -0600

    test: add unit tests for welcome email

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def welcome_email
+    @user = User.first
+    UserMailer.with(user: @user).welcome_email
+  end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#welcome_email" do
+    subject(:mail) { described_class.with(user:).welcome_email }
+
+    let(:user) { instance_double(User, name: "Josh Smith", email: "josh@email.com") }
+
+    it "renders the subject" do
+      expect(mail.subject).to eq("Welcome to Coffee Book!")
+    end
+
+    it "renders the receiver email" do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it "renders the sender email" do
+      sender = "notifications@example.com"
+      expect(mail.from).to eq([sender])
+    end
+  end
 end
