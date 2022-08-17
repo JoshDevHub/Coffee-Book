fe06b293b176f8b35826eb1e1d1b0ce13b32334a
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-17 11:51:22 -0600

    chore: remove default hello stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 import ToggleController from "./toggle_controller"
 
-application.register("hello", HelloController)
+import ToggleController from "./toggle_controller.js"
 application.register("toggle", ToggleController)
