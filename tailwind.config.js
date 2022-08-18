e54f7a32b1bb798616525e398f4ad23eb83d1507
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-18 02:02:53 -0600

    chore: change `md` breakpoint to better fit site's needs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,14 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '850px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    }
+  }
 }
