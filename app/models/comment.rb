6eb47f5b6c7c54a3f32b26f61613c1751b062780
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-26 09:36:02 -0600

    fix: add like_counter_cache column to comments table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,7 @@
 #  id               :bigint           not null, primary key
 #  body             :text
 #  commentable_type :string           not null
+#  likes_count      :integer
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #  commentable_id   :bigint           not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_042308) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_065317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_042308) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "commenter_id", null: false
+    t.integer "likes_count"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["commenter_id"], name: "index_comments_on_commenter_id"
   end
