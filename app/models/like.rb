88293b40e2e54f01c41a1cc9b9810aac89610279
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-26 09:39:27 -0600

    feat: add Notify to Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -19,8 +19,26 @@
 #  fk_rails_...  (liker_id => users.id)
 #
 class Like < ApplicationRecord
+  include Notify
+
   belongs_to :likeable, polymorphic: true, counter_cache: :likes_count
   belongs_to :liker, class_name: "User"
 
   has_many :notifications, as: :notifiable, dependent: :destroy
+
+  delegate :url_helpers, to: "Rails.application.routes"
+
+  private
+
+  def notification_action
+    "liked your #{likeable_type.downcase}."
+  end
+
+  def notification_path
+    resource = case likeable_type
+               when "Post" then likeable
+               when "Comment" then likeable.commentable
+               end
+    url_helpers.post_path(resource)
+  end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -21,5 +21,59 @@
 require "rails_helper"
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notify" do
+    let(:liker) { create(:user) }
+    let(:liked_user) { create(:user) }
+
+    context "when the like is on a post" do
+      subject(:post_like) { create(:like, liker:, likeable: post) }
+
+      let(:post) { create(:post, author: liked_user) }
+
+      it "creates a new notification for the user with the liked post" do
+        expect { post_like.notify(liker, liked_user) }
+          .to change(liked_user.notifications, :count).from(0).to(1)
+      end
+
+      it "creates a new notification with the expected message" do
+        expected_message = "#{liker.name} liked your post."
+        created_message = post_like.notify(liker, liked_user).message
+
+        expect(created_message).to eq(expected_message)
+      end
+
+      it "creates a new notification with the expected path" do
+        expected_url = "/posts/#{post.id}"
+        created_url = post_like.notify(liker, liked_user).url
+
+        expect(created_url).to eq(expected_url)
+      end
+    end
+
+    context "when the like is on a comment" do
+      subject(:comment_like) { create(:like, liker:, likeable: comment) }
+
+      let(:post) { create(:post) }
+      let(:comment) { create(:comment, commenter: liked_user, commentable: post) }
+
+      it "creates a new notification for the user with the liked comment" do
+        expect { comment_like.notify(liker, liked_user) }
+          .to change(liked_user.notifications, :count).from(0).to(1)
+      end
+
+      it "creates a new notification with the expected message" do
+        expected_message = "#{liker.name} liked your comment."
+        created_message = comment_like.notify(liker, liked_user).message
+
+        expect(created_message).to eq(expected_message)
+      end
+
+      it "creates a new notification with the expected url" do
+        expected_url = "/posts/#{post.id}"
+        created_url = comment_like.notify(liker, liked_user).url
+
+        expect(created_url).to eq(expected_url)
+      end
+    end
+  end
 end
