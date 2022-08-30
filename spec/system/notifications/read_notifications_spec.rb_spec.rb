6272ccd8c8fe79a8f7f55740a58a0e48609abb54
jmsmith1018 <jmsmith1018@gmail.com>
2022-08-29 20:31:58 -0600

    feat: add small notification badge to alert user to unread notifs

--- spec/system/notifications/read_notifications_spec.rb_spec.rb
+++ spec/system/notifications/read_notifications_spec.rb_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe "Reading notifications", type: :system do
   let(:current_user) { create(:user) }
 
   before do
-    build_list(:notification, 5, user: current_user)
+    create_list(:notification, 5, user: current_user)
     login_as(current_user)
   end
 
   it "displays symbol indicating notifications" do
     visit root_path
-    expect(page).to have_content("Notifications+")
+    expect(page).to have_selector("#notifications-badge")
   end
 
   it "removes symbol when notifications path is visited" do
     visit user_notifications_path(current_user)
-    expect(page).not_to have_content("Notifications+")
+    expect(page).not_to have_selector("#notifications-badge")
   end
 end
