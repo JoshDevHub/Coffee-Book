d3b2819aa259b401d12ef5ecb05d0197102080af
jmsmith1018 <jmsmith1018@gmail.com>
2022-09-29 14:16:23 -0600

    feat: use turbo to prevent full page refresh for like and unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,21 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.build(like_params)
     @like.notify(current_user, @like.likeable.author) if @like.save
-    redirect_back fallback_location: root_path
+
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path }
+      format.turbo_stream
+    end
   end
 
   def destroy
     @like = Like.find(params[:id])
     @like.destroy
 
-    redirect_back fallback_location: root_path
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path }
+      format.turbo_stream
+    end
   end
 
   private
