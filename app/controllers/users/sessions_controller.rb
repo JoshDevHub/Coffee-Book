4e3dcf21016fd8a559059cf022165b9f3a4b284f
jmsmith1018 <jmsmith1018@gmail.com>
2022-10-11 11:44:04 -0600

    chore: add `SessionsController` and configure after sign in path

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+end
