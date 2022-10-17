9e40596aa9ba77986d2cc0e4c28aafb0810ec38c
Johan Watelet <johanwatelet@gmail.com>
2022-10-17 21:59:44 +0200

    fix partial prefix missing and add test

---
+++ spec/system/users/visit_user_profile_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Visiting user profile", type: :system do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user, id: 5, first_name: "Bob", last_name: "Jones") }
+
+  before do
+    login_as(user)
+  end
+
+  context "when using user path with an user id" do
+    it "shows the profile" do
+      visit user_path(other_user)
+      expect(page).to have_content("#{other_user.first_name}'s Profile")
+    end
+  end
+end
