d8561c9e4b3bde5015046739fcf51660b6dcf375
jmsmith1018 <jmsmith1018@gmail.com>
2022-10-27 12:03:11 -0600

    feat: add `countSwitch()` function to `ToggleController`
    This function will change a class when a count has exceeded a
    certain value.

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,9 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static classes = [ "change" ];
-  static targets = [ "toggleElement" ];
+  static targets = [ "toggleElement", "count" ];
 
   switch() {
     this.toggleElementTarget.classList.toggle(this.changeClass);
+  }
+
+  countSwitch(event) {
+    const integerCount = Number(this.countTarget.textContent);
+    const elementHasClass = this.toggleElementTarget.classList.contains(this.changeClass);
+    const maxLength = event.params.max;
+
+    if (integerCount > maxLength && !elementHasClass) {
+      this.toggleElementTarget.classList.add(this.changeClass);
+    } else if (integerCount <= maxLength && elementHasClass) {
+      this.toggleElementTarget.classList.remove(this.changeClass);
+    }
   }
 }
