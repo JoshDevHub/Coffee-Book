1af7a36679575eb0a860e4569cfb31da4a4d7055
jmsmith1018 <jmsmith1018@gmail.com>
2022-11-05 16:53:32 -0600

    feat: add ability to update registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_signup_parameters, if: :devise_controller?
+  before_action :configure_user_update_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
+  # rubocop: disable Metrics/MethodLength
+  def configure_user_update_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(
+        :first_name,
+        :last_name,
+        :username,
+        :email,
+        :current_password,
+        :password,
+        :password_confirmation
+      )
+    end
+  end
+  # rubocop: enable Metrics/MethodLength
+
+  def configure_signup_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(
         :email,
