2456dfa0792f42d2d2d451b55fc0b9a032b62cba
Josh Smith <jmsmith1018@gmail.com>
2023-04-01 18:12:50 -0600

    chore: fix remaining rubocop offenses

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -5,9 +5,9 @@ class TurboDeviseController < ApplicationController
     def to_turbo_stream
       controller.render(options.merge(formats: :html))
     rescue ActionView::MissingTemplate => e
-      if get?
-        raise e
-      elsif has_errors? && default_action
+      raise e if get?
+
+      if has_errors? && default_action
         render rendering_options.merge(formats: :html, status: :unprocessable_entity)
       else
         redirect_to navigation_location
--- spec/components/profile_component_spec.rb
+++ spec/components/profile_component_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe ProfileComponent, type: :component do
-  it "renders the correct things" do
+  xit "renders the correct things" do
     pending "investigate ViewComponent testing"
   end
 end
